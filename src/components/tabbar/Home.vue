70b4ef4cb6eb3e384d6f194608b9ae7c3fa6a03d
Jewellin <JewellinChang@outlook.com>
2019-01-05 22:46:19 +0800

    add Swipe

--- src/components/tabbar/Home.vue
+++ src/components/tabbar/Home.vue
@@ -1,6 +1,11 @@
 <template>  
   <div>
    <h3>Home</h3>  
+   <mt-swipe :auto="2000">
+    <mt-swipe-item>1</mt-swipe-item>
+    <mt-swipe-item>2</mt-swipe-item>
+    <mt-swipe-item>3</mt-swipe-item>
+   </mt-swipe>
   </div>
 </template>
 
@@ -11,5 +16,12 @@ export default {
 </script>
 
 <style scoped>
-
+* {
+ margin: 0;
+ padding: 0;
+}
+.mint-swipe {
+ height: 200px;
+ background-color: #FF0;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -5,21 +5,25 @@ import animate from 'animate.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import { Badge } from 'element-ui'
-import { Button, Spinner, Header, Navbar, TabItem } from 'mint-ui'
+import { Button, Spinner, Header, Navbar, TabItem, Swipe, SwipeItem } from 'mint-ui'
 import App from './App'
 import router from './router'
 
+
 //element-ui的组件导入
-Vue.component(Badge.name, Badge);
+Vue.component(Badge.name, Badge)
 
 //mint-ui的组件导入
 Vue.component(Button.name, Button)
 Vue.component(Spinner.name, Spinner)
 Vue.component(Header.name, Header)
-Vue.component(Navbar.name, Navbar);
-Vue.component(TabItem.name, TabItem);
+Vue.component(Navbar.name, Navbar)
+Vue.component(TabItem.name, TabItem)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
 
 // Vue.use(MintUI)
+
 
 Vue.config.productionTip = false
 
